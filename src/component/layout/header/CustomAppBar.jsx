d935f0ae51d4e9e796bcc4067dbbafde2ff6d5a6
barami62 <wodbs7758@naver.com>
2024-01-12 11:51:42 +0900

    feat : 헤더 추가

---
+++ src/component/layout/header/CustomAppBar.jsx
@@ -0,0 +1,43 @@
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import CssBaseline from '@mui/material/CssBaseline'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import { useNavigate } from 'react-router-dom'
+
+const CustomAppBar = () => {
+  const navigate = useNavigate()
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <CssBaseline/>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography
+            onClick={() => navigate('/')}
+            variant="h6"
+            component="a"
+            sx={{
+              mr: 2,
+              fontWeight: 700,
+              color: 'inherit',
+              textDecoration: 'none',
+              flexGrow: 1,
+            }}
+          >
+            Gream
+          </Typography>
+          <Button color="inherit" onClick={() => navigate('/login')}>
+            Login
+          </Button>
+          <Button color="inherit" onClick={() => navigate('/signup')}>
+            Sign up
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  )
+}
+
+export default CustomAppBar
---
+++ src/component/layout/header/Header.jsx
@@ -0,0 +1,11 @@
+import CustomAppBar from './CustomAppBar.jsx'
+
+function Header () {
+  return (
+    <header>
+      <CustomAppBar/>
+    </header>
+  )
+}
+
+export default Header
