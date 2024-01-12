5f880f20ad129f9f1e521fb6c635bad75b3ffe8a
barami62 <wodbs7758@naver.com>
2024-01-12 11:52:24 +0900

    feat : 메인 페이지 추가

---
+++ src/page/MainPage.jsx
@@ -0,0 +1,14 @@
+import Layout from '../component/layout/Layout.jsx'
+
+function MainPage () {
+
+  return (
+    <>
+      <Layout>
+        <div>Hello in main page</div>
+      </Layout>
+    </>
+  )
+}
+
+export default MainPage
