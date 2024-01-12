d8255f7ec22f3a5f4cf1cdd30660e6ab2c6c2c47
barami62 <wodbs7758@naver.com>
2024-01-12 11:53:51 +0900

    feat : 리액트 쿼리 프로바이더 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import Router from './Router.jsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
 function App () {
   return (
     <>
-      <h1>Hello World!!!</h1>
+      <QueryClientProvider client={queryClient}>
+        <Router/>
+      </QueryClientProvider>
     </>
   )
 }
