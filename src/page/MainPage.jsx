20d1dae891ce924f276179cb43aff3ff3b50efd3
barami62 <wodbs7758@naver.com>
2024-01-14 14:51:06 +0900

    feat : 상품 리스트 표시 추가

--- src/page/MainPage.jsx
+++ src/page/MainPage.jsx
@@ -1,11 +1,30 @@
 import Layout from '../component/layout/Layout.jsx'
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+import ProductCard from '../component/ProductCard.jsx'
+import { List, ListItem } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
 
 function MainPage () {
+  const navigate = useNavigate()
+
+  const query = useQuery({
+    queryKey: ['products'],
+    queryFn: () => axios.get(`${import.meta.env.VITE_SERVER_URL}/api/products`)
+  })
 
   return (
     <>
       <Layout>
-        <div>Hello in main page</div>
+        <List sx={{ display: 'flex', flexDirection: 'row' }}>
+          {
+            query.isSuccess && query.data.data.data.map((product) =>
+              <ListItem onClick={() => navigate(`/products/${product.id}`)} key={product.id}>
+                <ProductCard product={product}/>
+              </ListItem>
+            )
+          }
+        </List>
       </Layout>
     </>
   )
