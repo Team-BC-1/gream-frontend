61d2ebbf7207bfcd7858965fc993e62cdc2fafdd
barami62 <wodbs7758@naver.com>
2024-01-16 19:52:06 +0900

    feat : 로그인 유저가 아닐 시 로그인 페이지로 이동하도록 추가

--- src/page/BuyPage.jsx
+++ src/page/BuyPage.jsx
@@ -3,28 +3,34 @@ import { useLocation, useNavigate, useParams } from 'react-router-dom'
 import Box from '@mui/material/Box'
 import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
-import { Card, CardContent } from '@mui/material'
+import { ButtonGroup, Card, CardContent, Divider } from '@mui/material'
 import { useEffect } from 'react'
 import useUserInfoStore from '../store/userInfo.js'
 import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
 
 function BuyPage () {
   const { productId } = useParams()
   const navigate = useNavigate()
   const { state } = useLocation()
+  const { nickname } = useUserInfoStore()
 
   useEffect(() => {
     if (state === null) {
       navigate(`/products/${productId}`)
     }
-  })
+  }, [])
+  useEffect(() => {
+    if (nickname === '') {
+      alert('로그인 후 접근 가능합니다.')
+      navigate('/login')
+    }
+  }, [])
 
   const querySellBid = useQuery({
     queryKey: ['sellHistory'],
     queryFn: () => axios.get(`${import.meta.env.VITE_SERVER_URL}/api/products/${productId}/sell`)
   })
-
-  const { nickname } = useUserInfoStore()
 
   if (querySellBid.isSuccess) {
     console.log(querySellBid.data.data.data)
@@ -37,12 +43,10 @@ function BuyPage () {
           <Box component={'div'} sx={{ display: 'flex', justifyContent: 'center' }}>
             <Card>
               <CardContent>
-
                 <Typography variant="body2" color="text.secondary">
                   {nickname}
                 </Typography>
               </CardContent>
-
             </Card>
           </Box>
         )
