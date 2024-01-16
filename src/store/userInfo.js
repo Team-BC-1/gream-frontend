f0804f1288c10780d8c9bb3682129ad7f4b99d91
barami62 <wodbs7758@naver.com>
2024-01-16 19:47:31 +0900

    feat : 새로고침 해도 유저 정보 유지하도록 추가

--- src/store/userInfo.js
+++ src/store/userInfo.js
@@ -1,22 +1,30 @@
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
-const useUserInfoStore = create((set) => ({
-  accessToken: '',
-  refreshToken: '',
-  nickname: '',
-  loginId: '',
-  setAccessToken: (accessToken) => set({
-    accessToken
-  }),
-  setRefreshToken: (refreshToken) => set({
-    refreshToken
-  }),
-  setNickname: (nickname) => set({
-    nickname
-  }),
-  setLoginId: (loginId) => set({
-    loginId
-  })
-}))
+const useUserInfoStore = create(
+  persist(
+    (set) => ({
+      accessToken: '',
+      refreshToken: '',
+      nickname: '',
+      loginId: '',
+      setAccessToken: (accessToken) => set({
+        accessToken
+      }),
+      setRefreshToken: (refreshToken) => set({
+        refreshToken
+      }),
+      setNickname: (nickname) => set({
+        nickname
+      }),
+      setLoginId: (loginId) => set({
+        loginId
+      })
+    }),
+    {
+      name: 'user-info-storage'
+    }
+  )
+)
 
 export default useUserInfoStore
