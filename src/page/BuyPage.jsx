64fdc5031b77dddb03b05195d11a4b141f1fadfe
barami62 <wodbs7758@naver.com>
2024-01-16 23:27:59 +0900

    feat : 구매 페이지 일부 완성

--- src/page/BuyPage.jsx
+++ src/page/BuyPage.jsx
@@ -3,17 +3,20 @@ import { useLocation, useNavigate, useParams } from 'react-router-dom'
 import Box from '@mui/material/Box'
 import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
-import { ButtonGroup, Card, CardContent, Divider } from '@mui/material'
-import { useEffect } from 'react'
+import { Card, CardContent, InputAdornment, Tab, Tabs } from '@mui/material'
+import { useEffect, useState } from 'react'
 import useUserInfoStore from '../store/userInfo.js'
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
 
 function BuyPage () {
   const { productId } = useParams()
   const navigate = useNavigate()
   const { state } = useLocation()
   const { nickname } = useUserInfoStore()
+  const [wantBuyPrice, setWantBuyPrice] = useState()
+  const [value, setValue] = useState(0)
 
   useEffect(() => {
     if (state === null) {
@@ -37,17 +40,73 @@ function BuyPage () {
   }
 
   return (
-    <Layout>
+    <Layout sx={{ display: 'flex', justifyContent: 'center' }}>
       {
         querySellBid.isSuccess && (
-          <Box component={'div'} sx={{ display: 'flex', justifyContent: 'center' }}>
-            <Card>
+          <Box component={'div'} sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            width: 500
+          }}
+          >
+            <Card sx={{ width: '100%' }}>
               <CardContent>
                 <Typography variant="body2" color="text.secondary">
                   {nickname}
                 </Typography>
               </CardContent>
             </Card>
+            <Box sx={{ flex: 1 }}>
+              <Box sx={{ flex: 1 }}>즉시 구매가 : 10,000원</Box>
+              <Box sx={{ flex: 1 }}>즉시 판매가 : 10,000원</Box>
+            </Box>
+            <Tabs value={value} onChange={(event, newValue) => setValue(newValue)} aria-label="basic tabs example">
+              <Tab label="구매 입찰"/>
+              <Tab label="즉시 구매"/>
+            </Tabs>
+
+            <Box value={value} index={0} hidden={value !== 0} sx={{ width: '100%' }}>
+              <TextField
+                id="buy-want-price"
+                label="구매 희망가"
+                variant="standard"
+                fullWidth
+                value={wantBuyPrice}
+                onChange
+                InputProps={{ endAdornment: <InputAdornment position="end">원</InputAdornment> }}
+              />
+              <Button
+                variant="contained"
+                fullWidth={true}
+                onClick={() => {}}
+                sx={{ backgroundColor: '#EF6253' }}
+              >
+                구매 입찰
+              </Button>
+            </Box>
+
+            <Box value={value} index={1} hidden={value !== 1} sx={{ width: '100%' }}>
+              <TextField
+                id="buy-want-price"
+                label="즉시 구매가"
+                variant="standard"
+                fullWidth
+                value={100000}
+                onChange
+                disabled
+                InputProps={{ endAdornment: <InputAdornment position="end">원</InputAdornment> }}
+              />
+              <Button
+                variant="contained"
+                fullWidth={true}
+                onClick={() => {}}
+                sx={{ backgroundColor: '#EF6253' }}
+              >
+                즉시 구매
+              </Button>
+            </Box>
           </Box>
         )
       }
