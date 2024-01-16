cbc80c75b8f004404291242dc974562e56cafafd
barami62 <wodbs7758@naver.com>
2024-01-16 17:29:07 +0900

    refactor : 컴포넌트 적용

--- src/page/ProductPage.jsx
+++ src/page/ProductPage.jsx
@@ -5,15 +5,15 @@ import Layout from '../component/layout/Layout.jsx'
 import Grid from '@mui/material/Grid'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
-import { DEFAULT_Y_AXIS_KEY, LineChart } from '@mui/x-charts'
 import { ButtonGroup, Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material'
-import dayjs from 'dayjs'
+import TradeLineChart from '../component/TradeLineChart.jsx'
+import TradeHistoryView from '../component/TradeHistoryView.jsx'
 
 function ProductPage () {
   const { productId } = useParams()
   const navigate = useNavigate()
 
-  const query = useQuery({
+  const queryProduct = useQuery({
     queryKey: ['product'],
     queryFn: () => axios.get(`${import.meta.env.VITE_SERVER_URL}/api/products/${productId}`)
   })
@@ -30,8 +30,8 @@ function ProductPage () {
     queryFn: () => axios.get(`${import.meta.env.VITE_SERVER_URL}/api/products/${productId}/buy`)
   })
 
-  if (query.isSuccess) {
-    console.log('product', query.data.data.data)
+  if (queryProduct.isSuccess) {
+    console.log('product', queryProduct.data.data.data)
   }
 
   if (queryTradeHistory.isSuccess) {
@@ -49,34 +49,44 @@ function ProductPage () {
   return (
     <Layout>
       {
-        query.isSuccess && queryBuyBid.isSuccess && querySellBid.isSuccess && queryTradeHistory.isSuccess && (
+        queryProduct.isSuccess && queryBuyBid.isSuccess && querySellBid.isSuccess && queryTradeHistory.isSuccess && (
           <Grid container spacing={2}>
             <Grid item sm={12} md={6} sx={{ display: 'flex', justifyContent: 'center', padding: 10 }}>
               <Box
                 component="img"
-                src={query.data.data.data.imageUrl}
+                src={queryProduct.data.data.data.imageUrl}
                 sx={{ maxWidth: '100%' }}
               />
             </Grid>
             <Grid item sm={12} md={6}>
               <Box component={'h1'}>
-                {query.data.data.data.name}
+                {queryProduct.data.data.data.name}
               </Box>
               <Box component={'h3'}>
-                {query.data.data.data.brand}
+                {queryProduct.data.data.data.brand}
               </Box>
               <Grid container spacing={1}>
                 <Grid item xs={6}>
                   <Button
                     variant="contained"
                     fullWidth={true}
-                    sx={{ backgroundColor: '#EF6253' }}>구매</Button>
+                    onClick={() => navigate(`/buy/${productId}`, {
+                      state: { product: queryProduct.data.data.data }
+                    })}
+                    sx={{ backgroundColor: '#EF6253' }}
+                  >
+                    구매
+                  </Button>
                 </Grid>
                 <Grid item xs={6}>
                   <Button
                     variant="contained"
                     fullWidth={true}
-                    sx={{ backgroundColor: '#41B979' }}>
+                    onClick={() => navigate(`/sell/${productId}`, {
+                      state: { product: queryProduct.data.data.data }
+                    })}
+                    sx={{ backgroundColor: '#41B979' }}
+                  >
                     판매
                   </Button>
                 </Grid>
@@ -89,19 +99,10 @@ function ProductPage () {
                   </Button>
                 </Grid>
                 <Grid item xs={12}>
-                  <LineChart
-                    series={[
-                      {
-                        data: queryTradeHistory.data.data.data.map(trade => trade.finalPrice),
-                        showMark: false,
-                        color: '#F48174'
-                      }
-                    ]}
-                    height={300}
-                    bottomAxis={null}
-                    leftAxis={null}
-                    rightAxis={DEFAULT_Y_AXIS_KEY}
-                  />
+                  {
+                    queryTradeHistory.isSuccess &&
+                    <TradeLineChart tradeList={queryTradeHistory.data.data.data.map(trade => trade.finalPrice)}/>
+                  }
                 </Grid>
                 <Grid item xs={12}>
                   <ButtonGroup fullWidth={true}>
@@ -119,13 +120,7 @@ function ProductPage () {
                     <TableBody>
                       {
                         queryTradeHistory.isSuccess &&
-                        queryTradeHistory.data.data.data
-                          .map((ele) => (
-                            <TableRow key={ele.id}>
-                              <TableCell>{addCommasAtMoney(ele.finalPrice)}</TableCell>
-                              <TableCell align={'right'}>{dayjs(ele.tradeDate).format('YYYY/MM/DD')}</TableCell>
-                            </TableRow>
-                          ))
+                        <TradeHistoryView tradeHistoryList={queryTradeHistory.data.data.data}/>
                       }
                     </TableBody>
                   </Table>
@@ -137,10 +132,6 @@ function ProductPage () {
       }
     </Layout>
   )
-}
-
-function addCommasAtMoney (money) {
-  return money.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',') + '원'
 }
 
 export default ProductPage
