a81686d560f569879219c521fc6ce6eb03e10a76
barami62 <wodbs7758@naver.com>
2024-01-18 14:55:57 +0900

    feat : 판매 진행 중 및 완료 컴포넌트 추가

---
+++ src/component/SellEndHistoryList.jsx
@@ -0,0 +1,45 @@
+import Box from '@mui/material/Box'
+import { Card, CardContent, List, ListItem } from '@mui/material'
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+import useUserInfoStore from '../store/userInfo.js'
+
+function SellEndHistoryList () {
+  const { accessToken, refreshToken } = useUserInfoStore()
+
+  const queryUserSellEndHistory = useQuery({
+    queryKey: ['userSellEndHistory'],
+    queryFn: () => axios.get(`${import.meta.env.VITE_SERVER_URL}/api/sell/history/end`,
+      {
+        headers: {
+          'Access-Token': accessToken,
+          'Refresh-Token': refreshToken
+        }
+      })
+  })
+
+  return (
+    <>
+      <Box component={'h2'}>판매 완료</Box>
+      <List sx={{ marginBottom: 10, display: 'flex', flexDirection: 'row', overflow: 'scroll' }}>
+        {
+          queryUserSellEndHistory.isSuccess && (
+            queryUserSellEndHistory.data.data.data.map(history =>
+              (
+                <ListItem key={history.orderId}>
+                  <Card sx={{ fontSize: 15, marginX: 1 }}>
+                    <CardContent>brand : {history.brand}</CardContent>
+                    <CardContent>name : {history.name}</CardContent>
+                    <CardContent>finalPrice : {history.finalPrice}</CardContent>
+                  </Card>
+                </ListItem>
+              )
+            )
+          )
+        }
+      </List>
+    </>
+  )
+}
+
+export default SellEndHistoryList
---
+++ src/component/SellOnprogressHistoryList.jsx
@@ -0,0 +1,46 @@
+import Box from '@mui/material/Box'
+import { Card, CardContent, List, ListItem } from '@mui/material'
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+import useUserInfoStore from '../store/userInfo.js'
+
+function SellOnprogressHistoryList () {
+  const { accessToken, refreshToken } = useUserInfoStore()
+
+  const queryUserSellOnprogressHistory = useQuery({
+    queryKey: ['userSellOnprogressHistory'],
+    queryFn: () => axios.get(`${import.meta.env.VITE_SERVER_URL}/api/sell/history/onprogress`,
+      {
+        headers: {
+          'Access-Token': accessToken,
+          'Refresh-Token': refreshToken
+        }
+      })
+  })
+
+  return (
+    <>
+      <Box component={'h2'}>진행중인 판매 입찰</Box>
+      <List sx={{ marginBottom: 10, display: 'flex', flexDirection: 'row', overflow: 'scroll' }}>
+        {
+          queryUserSellOnprogressHistory.isSuccess && (
+            queryUserSellOnprogressHistory.data.data.data.map(history =>
+              (
+                <ListItem key={history.orderId}>
+                  <Card sx={{ fontSize: 15, marginX: 1 }}>
+                    <CardContent>productBrand : {history.productBrand}</CardContent>
+                    <CardContent>productName : {history.productName}</CardContent>
+                    <CardContent>sellPrice : {history.sellPrice}</CardContent>
+                    <CardContent>bidDeadlineAt : {history.bidDeadlineAt}</CardContent>
+                  </Card>
+                </ListItem>
+              )
+            )
+          )
+        }
+      </List>
+    </>
+  )
+}
+
+export default SellOnprogressHistoryList
