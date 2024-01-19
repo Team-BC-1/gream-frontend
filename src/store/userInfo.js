31c0e267835a1b0ead37654edf40446fbacf3c46
barami62 <wodbs7758@naver.com>
2024-01-19 12:31:16 +0900

    feat : 관심 상품 추가 및 삭제 기능 추가

--- src/store/userInfo.js
+++ src/store/userInfo.js
@@ -8,10 +8,14 @@ const useUserInfoStore = create(
       refreshToken: '',
       nickname: '',
       loginId: '',
+      likes: [],
       setAccessToken: (accessToken) => set({ accessToken }),
       setRefreshToken: (refreshToken) => set({ refreshToken }),
       setNickname: (nickname) => set({ nickname }),
-      setLoginId: (loginId) => set({ loginId })
+      setLoginId: (loginId) => set({ loginId }),
+      setLikes: (likes) => set({ likes: [...likes] }),
+      addLike: (like) => set(state => ({ likes: [...state.likes, like] })),
+      deleteLike: (like) => set(state => ({ likes: state.likes.filter(l => l !== like) })),
     }),
     {
       name: 'user-info-storage'
