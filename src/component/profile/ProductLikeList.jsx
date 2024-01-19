c9c84d2f8b1346feef671727462d9a45954aca68
barami62 <wodbs7758@naver.com>
2024-01-19 14:22:40 +0900

    feat : 관심상품 클릭 시 해당 상품 상세 페이지로 이동하도록 기능 구현

--- src/component/profile/ProductLikeList.jsx
+++ src/component/profile/ProductLikeList.jsx
@@ -3,8 +3,10 @@ import { Card, CardContent, List, ListItem } from '@mui/material'
 import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 import useUserInfoStore from '../../store/userInfo.js'
+import { useNavigate } from 'react-router-dom'
 
 function ProductLikeList () {
+  const navigate = useNavigate()
   const { accessToken, refreshToken } = useUserInfoStore()
 
   const queryProductLikes = useQuery({
@@ -32,7 +34,7 @@ function ProductLikeList () {
           queryProductLikes.isSuccess && (
             queryProductLikes.data.data.data.map(product =>
               (
-                <ListItem key={product.id}>
+                <ListItem key={product.id} onClick={() => navigate(`/products/${product.id}`)}>
                   <Card sx={{ fontSize: 15, marginX: 1, width: 200 }}>
                     <CardContent>brand : {product.brand}</CardContent>
                     <CardContent>name : {product.name}</CardContent>
