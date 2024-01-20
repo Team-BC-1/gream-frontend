647b735579c39509424d4767f95b168db01b5366
barami62 <wodbs7758@naver.com>
2024-01-21 03:01:58 +0900

    feat : 구매 취소 기능 추가

--- src/component/profile/BuyOnprogressHistoryList.jsx
+++ src/component/profile/BuyOnprogressHistoryList.jsx
@@ -1,8 +1,9 @@
 import Box from '@mui/material/Box'
 import { Card, CardContent, List, ListItem } from '@mui/material'
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 import useUserInfoStore from '../../store/userInfo.js'
+import Button from '@mui/material/Button'
 
 function BuyOnprogressHistoryList () {
   const { accessToken, refreshToken } = useUserInfoStore()
@@ -18,6 +19,22 @@ function BuyOnprogressHistoryList () {
       })
   })
 
+  const mutationCancelBuy = useMutation(({
+    mutationFn: (buyId) => axios.delete(
+      `${import.meta.env.VITE_SERVER_URL}/api/buy/bid/${buyId}`,
+      {
+        headers: {
+          'Access-Token': accessToken,
+          'Refresh-Token': refreshToken
+        }
+      }),
+    onSuccess: () => {
+      alert('구매 입찰 취소 성공!')
+      queryUserBuyOnprogressHistory.refetch()
+    },
+    onError: error => alert(error.response.data.message)
+  }))
+
   return (
     <>
       <Box component={'h2'}>진행중인 구매 입찰</Box>
@@ -32,6 +49,14 @@ function BuyOnprogressHistoryList () {
                     <CardContent>productName : {history.productName}</CardContent>
                     <CardContent>discountPrice : {history.discountPrice}</CardContent>
                     <CardContent>price : {history.price}</CardContent>
+                    <Button
+                      variant="contained"
+                      disabled={mutationCancelBuy.isPending}
+                      onClick={() => mutationCancelBuy.mutate(history.buyId)}
+                      sx={{ backgroundColor: '#EF6253' }}
+                    >
+                      입찰 취소
+                    </Button>
                   </Card>
                 </ListItem>
               )
