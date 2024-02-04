20aaaed2336d88f452fa1ed0607463e14bdefb77
barami62 <wodbs7758@naver.com>
2024-02-04 23:17:15 +0900

    feat : 상품 검색 텍스트필드 추가

--- src/page/MainPage.jsx
+++ src/page/MainPage.jsx
@@ -1,28 +1,55 @@
 import Layout from '../component/layout/Layout.jsx'
 import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
-import ProductCard from '../component/ProductCard.jsx'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useSearchParams } from 'react-router-dom'
 import Grid from '@mui/material/Grid'
+import { useEffect, useState } from 'react'
+import TextField from '@mui/material/TextField'
+import ProductView from '../component/main/ProductView.jsx'
+import { Autocomplete } from '@mui/material'
+import Box from '@mui/material/Box'
 
 function MainPage () {
   const navigate = useNavigate()
+  const [searchValue, setSearchValue] = useState('')
+  const [searchParams] = useSearchParams()
+  const name = searchParams.get('name')
+  console.log('searchParams', searchParams.get('name'))
 
   const query = useQuery({
-    queryKey: ['products'],
-    queryFn: () => axios.get(`${import.meta.env.VITE_SERVER_URL}/api/products`)
+    queryKey: ['products', name],
+    queryFn: () => axios.get(`${import.meta.env.VITE_SERVER_URL}/api/products${name ? `?name=${name}` : ''}`)
   })
+
+  useEffect(() => {
+    if (name === null) {
+      setSearchValue('')
+    }
+  }, [])
+
+  const submitHandler = (event) => {
+    event.preventDefault()
+    navigate(`/?name=${searchValue}`)
+  }
 
   return (
     <>
       <Layout>
+        <Box component="form" onSubmit={submitHandler}>
+          <Autocomplete
+            freeSolo
+            options={[]}
+            sx={{ width: 300, marginLeft: 'auto', marginBottom: 10 }}
+            renderInput={(params) =>
+              <TextField
+                {...params}
+                label="상품검색"/>}
+            onChange={(event, value) => setSearchValue(value)}
+          />
+        </Box>
         <Grid container spacing={2}>
           {
-            query.isSuccess && query.data.data.data.map((product) =>
-              <Grid item md={2} xs={6} onClick={() => navigate(`/products/${product.id}`)} key={product.id}>
-                <ProductCard product={product}/>
-              </Grid>
-            )
+            query.isSuccess && <ProductView products={query.data.data.data}/>
           }
         </Grid>
       </Layout>
