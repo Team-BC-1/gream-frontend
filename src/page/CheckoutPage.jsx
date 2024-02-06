81216b7b7e1ec1a13fc2acd813cf85aa9f761c8d
barami62 <wodbs7758@naver.com>
2024-02-06 18:06:26 +0900

    feat : 결제 페이지에 레이아웃 적용

--- src/page/CheckoutPage.jsx
+++ src/page/CheckoutPage.jsx
@@ -1,13 +1,16 @@
 import { PaymentCheckout } from '../component/payments/PaymentCheckout.jsx'
 import { useSearchParams } from 'react-router-dom'
+import Layout from '../component/layout/Layout.jsx'
 
 export function CheckoutPage () {
   const [searchParams] = useSearchParams()
   const point = searchParams.get('point')
 
   return (
-    <PaymentCheckout
-      price={point}
-    />
+    <Layout>
+      <PaymentCheckout
+        price={point}
+      />
+    </Layout>
   )
 }
